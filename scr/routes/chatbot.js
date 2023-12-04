26a5b01594442a02f127c07753f51f824cdb4019
Alan Andre <114703935+AlanAndreUP@users.noreply.github.com>
2023-12-03 20:32:30 -0600

    Add files via upload

--- scr/routes/chatbot.js
+++ scr/routes/chatbot.js
@@ -1,4 +1,3 @@
-
 function getInitialMessage() {
     return "Hola, soy tu Chatbot. Por favor, elige una opción:\n" +
            "1. Como se agenda una cita.\n" +
@@ -9,21 +8,35 @@ function getInitialMessage() {
 }
 
 function processMessage(userInput) {
+    let response;
 
     switch (userInput.toString()) {
         case '1':
-            return "Para Agendar una cita debes de ir a Perfil, darle boton al icono de calendario ahi podras ingresar los datos de la cita.";
+            response = "Para Agendar una cita debes de ir a Perfil, darle boton al icono de calendario ahi podras ingresar los datos de la cita.";
+            break;
         case '2':
-            return "Para modificar una cita, en Perfil veras tus citas actuales y veras un lapiz para editar dale ahi en la cita que desees editar.";
+            response = "Para modificar una cita, en Perfil veras tus citas actuales y veras un lapiz para editar dale ahi en la cita que desees editar.";
+            break;
         case '3':
-            return "No te preocupes esto es normal, te apareceran las notas hasta que despues de tu cita, pero OJO solo veras la nota de la ultima cita.";
+            response = "No te preocupes esto es normal, te apareceran las notas hasta que despues de tu cita, pero OJO solo veras la nota de la ultima cita.";
+            break;
         case '4':
-            return "Si, si puedes en perfil veras un boton de menos en cada cita que tengas, dale Click ahi y podras eliminar la cita .";
+            response = "Si, si puedes en perfil veras un boton de menos en cada cita que tengas, dale Click ahi y podras eliminar la cita.";
+            break;
         case '5':
-            return "Tranquilo, podras editar tu cita Siempre y cuando la Cita no este confirmada, Si es el caso que aun no esta confirmada sigue los pasos de la pregunta 2.";
+            response = "Tranquilo, podras editar tu cita Siempre y cuando la Cita no este confirmada, Si es el caso que aun no esta confirmada sigue los pasos de la pregunta 2.";
+            break;
         default:
-            return "Lo siento, no entendí eso. Por favor, elige una opción entre 1 y 5.";
+            response = "Lo siento, no entendí eso. Por favor, elige una opción entre 1 y 5.";
+            break;
     }
+
+    return response + "\n\n¿Te puedo ayudar en otra cosa? Elige una opción:\n" +
+           "1. Como se agenda una cita.\n" +
+           "2. Como modifico una cita.\n" +
+           "3. No me aparecen ninguna nota después de agendar una cita.\n" +
+           "4. ¿Puedo cancelar una o más citas?.\n" +
+           "5. ¡Puse información errónea! ¿Qué hago.";
 }
 
 module.exports = { getInitialMessage, processMessage };
