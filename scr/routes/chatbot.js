48dcab2f55fd6e570bb812cdcc3c06c71bbdd27b
Alan Andre <114703935+AlanAndreUP@users.noreply.github.com>
2023-12-03 20:27:44 -0600

    Add files via upload

--- scr/routes/chatbot.js
+++ scr/routes/chatbot.js
@@ -12,15 +12,15 @@ function processMessage(userInput) {
 
     switch (userInput.toString()) {
         case '1':
-            return "a.";
+            return "Para Agendar una cita debes de ir a Perfil, darle boton al icono de calendario ahi podras ingresar los datos de la cita.";
         case '2':
-            return "a.";
+            return "Para modificar una cita, en Perfil veras tus citas actuales y veras un lapiz para editar dale ahi en la cita que desees editar.";
         case '3':
-            return "a.";
+            return "No te preocupes esto es normal, te apareceran las notas hasta que despues de tu cita, pero OJO solo veras la nota de la ultima cita.";
         case '4':
-            return "a.";
+            return "Si, si puedes en perfil veras un boton de menos en cada cita que tengas, dale Click ahi y podras eliminar la cita .";
         case '5':
-            return "a.";
+            return "Tranquilo, podras editar tu cita Siempre y cuando la Cita no este confirmada, Si es el caso que aun no esta confirmada sigue los pasos de la pregunta 2.";
         default:
             return "Lo siento, no entendí eso. Por favor, elige una opción entre 1 y 5.";
     }
